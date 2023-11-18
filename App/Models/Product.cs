1d7097d97b7543245ff5c2eea32870320ed556af
Evaneta <eftimovaevaneta@gmail.com>
2023-11-18 17:31:49 +0200

    product model added

---
+++ App/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Quantity { get; set; }
+        public Brands Brand { get; set; }
+    }
+}
