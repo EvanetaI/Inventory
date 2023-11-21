10558b1fb853da7e6ce4953d1f1399151ef9979d
Evaneta <eftimovaevaneta@gmail.com>
2023-11-21 22:24:13 +0200

    some updates

---
+++ App/Dtos/Product/AddProductDto.cs
@@ -0,0 +1,11 @@
+﻿using App.Models;
+
+namespace App.Dtos.Product
+{
+    public class AddProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Quantity { get; set; }
+        public Brands Brand { get; set; }
+    }
+}
---
+++ App/Dtos/Product/GetProductDto.cs
@@ -0,0 +1,12 @@
+﻿using App.Models;
+
+namespace App.Dtos
+{
+    public class GetProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Quantity { get; set; }
+        public Brands Brand { get; set; }
+    }
+}
