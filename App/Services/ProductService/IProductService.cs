c8712d39d6e8b83376e511f8f48f0a9103b8d6df
Evaneta <eftimovaevaneta@gmail.com>
2023-11-21 22:08:51 +0200

    some update

---
+++ App/Services/ProductService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using App.Dtos;
+using App.Dtos.Product;
+using App.Models;
+
+namespace App.Services.ProductService
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse<List<GetProductDto>>> GetAllProducts();
+        Task<ServiceResponse<GetProductDto>> GetProductByName(string name);
+        Task<ServiceResponse<List<GetProductDto>>> AddProduct(AddProductDto newCharacter);
+        Task<ServiceResponse<GetProductDto>> UpdateProduct(GetProductDto updatedCharacter);
+        Task<ServiceResponse<List<GetProductDto>>> DeleteProduct(int id);
+    }
+}
---
+++ App/Services/ProductService/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using App.Dtos;
+using App.Dtos.Product;
+using App.Models;
+
+namespace App.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public Task<ServiceResponse<List<GetProductDto>>> AddProduct(AddProductDto newCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<GetProductDto>>> DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<GetProductDto>>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<GetProductDto>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<GetProductDto>> UpdateProduct(GetProductDto updatedCharacter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
