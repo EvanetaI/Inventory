2c69001fbb4d9cd0f3dc2bbe3c39ef9afc8b938b
Evaneta <eftimovaevaneta@gmail.com>
2023-12-07 21:10:19 +0200

    Create DataContext.cs

---
+++ App/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
